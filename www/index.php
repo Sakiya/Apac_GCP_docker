8498d059d57c923c0dfbf1dc2c5ceb3ea98abed4
Sakiya Pon <ki8.m21b@gmail.com>
2026-06-02 02:14:44 +0800

    chore: update internationalization data, documentation translations, and project assets

--- www/index.php
+++ www/index.php
@@ -5,9 +5,10 @@ $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', filter_var(getenv('YII_DEBUG') ?: true, FILTER_VALIDATE_BOOLEAN));
+defined('YII_DEBUG') || define('YII_DEBUG', filter_var(getenv('YII_DEBUG') ?: true, FILTER_VALIDATE_BOOLEAN));
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', (int)(getenv('YII_TRACE_LEVEL') ?: 9));
+defined('YII_TRACE_LEVEL') || define('YII_TRACE_LEVEL', (int)(getenv('YII_TRACE_LEVEL') ?: 9));
 
-require_once($yii);
+require_once $yii;
 Yii::createWebApplication($config)->run();
+
--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -29,6 +29,7 @@
     <?php //Yii::app()->clientScript->registerCssFile("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");?>
     <?php //Yii::app()->clientScript->registerCssFile("https://jeremyfagis.github.io/dropify/dist/css/dropify.min.css");?>
 	<?php Yii::app()->clientScript->registerCssFile(Html::cssUrl('formVarify.css?' . $expire, 'screen'));?>
+	<?php Yii::app()->clientScript->registerScriptFile(Html::scriptUrl('jquery-parsley.js?' . $expire), CClientScript::POS_HEAD);?>
 	<?php Yii::app()->clientScript->registerScriptFile(Html::scriptUrl('app.js?' . $expire), CClientScript::POS_HEAD);?>
 	<?php //Yii::app()->clientScript->registerScriptFile("https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js", CClientScript::POS_HEAD);?> 
 	<?php Yii::app()->clientScript->registerScriptFile("https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js", CClientScript::POS_HEAD);?> 
@@ -36,10 +37,7 @@
 	<?php Yii::app()->clientScript->registerScriptFile(Html::scriptUrl('sweetalert.js'));?>
     <!--============================== css close ==============================-->
     <!--============================== script start ==============================-->
-	<meta http-equiv="Content-Security-Policy" content="Content-Security-Policy: default-src 'self';script-src 'self' *.ajax.googleapis.com *.googletagmanager.com *.google-analytics.com 'unsafe-inline' 'unsafe-eval';connect-src 'self' *.google-analytics.com 'unsafe-inline';style-src 'self' 'unsafe-inline';style-src-elem 'self' 'unsafe-inline' data:;img-src 'self' 'unsafe-inline' *.google-analytics.com blob: data:;font-src 'self' 'unsafe-inline' data:;
-frame-ancestors 'none'; 
-block-all-mixed-content; 
-upgrade-insecure-requests;">
+	<meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.google.com https://*.gstatic.com https://*.googletagmanager.com https://*.ajax.googleapis.com https://*.google-analytics.com *.google.com *.gstatic.com *.googletagmanager.com *.ajax.googleapis.com *.google-analytics.com; connect-src 'self' https://*.google-analytics.com https://*.google.com *.google-analytics.com *.google.com; style-src 'self' 'unsafe-inline' https://*.googleapis.com *.googleapis.com; style-src-elem 'self' 'unsafe-inline' data: https://*.googleapis.com https://*.gstatic.com *.googleapis.com *.gstatic.com; img-src 'self' 'unsafe-inline' https://*.google-analytics.com https://*.google.com https://*.gstatic.com *.google-analytics.com *.google.com *.gstatic.com blob: data:; font-src 'self' 'unsafe-inline' data: https://*.gstatic.com *.gstatic.com; frame-src 'self' https://*.google.com *.google.com; block-all-mixed-content;">
 	<?php $this->renderPartial('/layouts/Analytics'); ?> 
 </head>
 
